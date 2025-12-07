aed8ab44389947f1ae78fb3ac7d0286fef65380a
Anass El Khatmi <114924282+Anass-KH@users.noreply.github.com>
2025-12-07 23:02:04 +0100

    #5 #7 #6 Defer Attribuut

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-// 1. Wacht tot het hele document geladen is zodat alle HTML beschikbaar is
-document.addEventListener("DOMContentLoaded", () => {
-
-  // 2. Zoek de hamburgerknop op de pagina (die opent/sluit het menu)
+  // 1. Zoek de hamburgerknop op de pagina (die opent/sluit het menu)
   const hamburger = document.querySelector(".hamburger");
 
-  // 3. Zoek de navigatielijst die wordt getoond 
+  // 2. Zoek de navigatielijst die wordt getoond 
   const nav = document.querySelector("#navList");
 
-  // 4. Click op het Hamburger menu 
+  // 3. Click op het Hamburger menu 
   hamburger.addEventListener("click", () => {
 
-    // 5. Voegt of verwijderd de class open Wanneer open aanwezig is het menu zichtbaar zo niet is het hidden.
+    // 4. Voegt of verwijderd de class open Wanneer open aanwezig is het menu zichtbaar zo niet is het hidden.
     nav.classList.toggle("open");
 
   });
-});
