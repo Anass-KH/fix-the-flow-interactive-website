eb00a485870c141f5037d5df21c81857c85912ed
Anass El Khatmi <114924282+Anass-KH@users.noreply.github.com>
2025-12-16 11:02:31 +0100

    #5 Hamburger menu  sluiten

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-  // 1. Zoek de hamburgerknop op de pagina (die opent/sluit het menu)
-  const hamburger = document.querySelector(".hamburger");
+// 1. Selecteer de hamburgerknop (menu openen)
+const hamburger = document.querySelector(".hamburger");
 
-  // 2. Zoek de navigatielijst die wordt getoond 
-  const nav = document.querySelector("#navList");
+// 2. Selecteer de sluitknop (menu sluiten)
+const closeButton = document.querySelector(".menu-close");
 
-  // 3. Click op het Hamburger menu 
-  hamburger.addEventListener("click", () => {
+// 3. Selecteer het navigatiemenu
+const nav = document.querySelector("#navList");
 
-    // 4. Voegt of verwijderd de class open Wanneer open aanwezig is het menu zichtbaar zo niet is het hidden.
-    nav.classList.toggle("open");
+// 4. Klik op hamburger om het menu te openen
+hamburger.addEventListener("click", () => {
+  // Hamburger menu is zichtbaar
+  nav.classList.add("open");  
+  // hamburger menu wordt verborgen        
+  hamburger.classList.add("hidden");   
+  // X wordt getoond want hidden wordt verwijderd 
+  closeButton.classList.remove("hidden");
+});
 
-  });
+// 5. Klik op X om het menu te sluiten
+closeButton.addEventListener("click", () => {
+  // sluit het menu en verwijderd de class open
+  nav.classList.remove("open");  
+  // Verbergt de close button      
+  closeButton.classList.add("hidden");
+  // hamburger menu wordt getoond
+  hamburger.classList.remove("hidden"); // hamburger tonen
+});
